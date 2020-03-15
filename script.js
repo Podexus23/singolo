607495ebe170de5e28d7a2b575310c5ffc76bdc9
Podexus23 <santon23@gmail.com>
2020-03-15 22:28:11 +0300

    fix: header, protfolio; feat: added modal window
    fix: fixed header position
    fix: fixed portfolio block, no empty block for pictures and all of them are fully, absolutely randomized ;-)
    refactor: little changes for Services block icons, now it looks better, and dont disturb Header

--- script.js
+++ script.js
@@ -26,31 +26,84 @@ function switchSecondPhone() {
 }
 
 function togglePictureBlock(filterName) {
-    document.querySelectorAll(".picture-block").forEach(item => item.style.display="none");
+    document.querySelectorAll(".picture-block").forEach(item => item.style.order="4");
     document.querySelectorAll(".tag").forEach(item => item.classList.remove("active"));
     
     switch (filterName) {
         case "all":
-            document.querySelectorAll(".picture-block").forEach(item => item.style.display="block");
             document.querySelector(".all-tag").classList.add("active")
             break;
 
         case "web":
-            document.querySelectorAll(".web").forEach(item => item.style.display="block");
+            document.querySelectorAll(".web").forEach(item => item.style.order="1");
+            document.querySelectorAll(".art").forEach(item => item.style.order="2");
             document.querySelector(".web-tag").classList.add("active")
         break;
 
         case "graph":
-            document.querySelectorAll(".graph").forEach(item => item.style.display="block");
+            document.querySelectorAll(".graph").forEach(item => item.style.order="1");
+            document.querySelectorAll(".art").forEach(item => item.style.order="2");
+            document.querySelectorAll(".second-pic").forEach(item => item.style.order="1");
             document.querySelector(".graph-tag").classList.add("active")
         break;
 
         case "art":
-            document.querySelectorAll(".art").forEach(item => item.style.display="block");
+            document.querySelectorAll(".art").forEach(item => item.style.order="1");
+            document.querySelectorAll(".first-pic").forEach(item => item.style.order="2");
+            document.querySelectorAll(".web").forEach(item => item.style.order="3");
+            document.querySelectorAll(".third-pic").forEach(item => item.style.order="2");
             document.querySelector(".art-tag").classList.add("active")
         break;
     
         default:
             break;
     }
+}
+
+function makeActive(event) {
+    if(event.target.parentNode.classList.contains('active')) {
+        event.target.parentNode.classList.remove('active');
+    } else {
+        document.querySelectorAll(".picture-block").forEach(item => item.classList.remove('active'));
+        event.target.parentNode.classList.add('active');
+    }
+}
+
+
+let modalWindow = document.getElementById('modal-window')
+let formForCheck = document.forms[0];
+
+
+function openModal() {
+    if (!formForCheck.name.value.length) {
+        alert('Please fill name field');
+    } else if (!formForCheck.email.value.length ||
+     !formForCheck.email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+        alert('Please check email adress');
+    } else {
+      document.getElementById('subject-text').innerHTML = formForCheck.subject.value ? `Тема: ${formForCheck.subject.value}` : 'Без темы';
+      document.getElementById('description-text').innerHTML = formForCheck.description.value ? `Описание: ${formForCheck.description.value}` : 'Без описания';
+      modalWindow.style.display='block';
+    }
+}
+
+function closeModal() {
+    modalWindow.style.display='none'
+}
+
+window.onclick = function(event) {
+  if (event.target == modalWindow) {
+    modalWindow.style.display = "none";
+  }
+}
+
+function changeActiveHeaderElement() {
+    document.querySelectorAll('.header-element').forEach(item => item.classList.remove('header-active'));
+}
+
+function setActiveElement(event) {
+    setTimeout(function() {
+        document.querySelectorAll('.header-element').forEach(item => item.classList.remove('header-active'));
+        event.target.parentNode.classList.add('header-active');
+    }, 100)
 }
