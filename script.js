131c0e97dcf4ee6897db7d1e5d1abde92e6f3a3f
Podexus23 <santon23@gmail.com>
2020-03-20 23:13:22 +0300

    feat: smooth header, submit use reset

--- script.js
+++ script.js
@@ -88,11 +88,13 @@ function openModal() {
 }
 
 function closeModal() {
-    modalWindow.style.display='none'
+    modalWindow.style.display='none';
+    formForCheck.reset();
 }
 
 window.onclick = function(event) {
   if (event.target == modalWindow) {
+    formForCheck.reset();  
     modalWindow.style.display = "none";
   }
 }
@@ -101,9 +103,33 @@ function changeActiveHeaderElement() {
     document.querySelectorAll('.header-element').forEach(item => item.classList.remove('header-active'));
 }
 
-function setActiveElement(event) {
-    setTimeout(function() {
-        document.querySelectorAll('.header-element').forEach(item => item.classList.remove('header-active'));
-        event.target.parentNode.classList.add('header-active');
-    }, 100)
+function setActiveElement(event, elementId) {
+    document.querySelector(elementId).scrollIntoView({behavior:"smooth", block:"start"});
 }
+
+let mainNavLinks = document.querySelectorAll(".header-element");
+
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
+    mainNavLinks.forEach(link => { 
+     let section = document.querySelector(link.dataset.target);
+      
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        link.classList.add("header-active");
+      } else {
+        link.classList.remove("header-active");
+     }
+     if (event.target.body.clientHeight + event.target.body.scrollTop >= event.target.body.scrollHeight) {
+        mainNavLinks[mainNavLinks.length-1].classList.add("header-active");
+        mainNavLinks[mainNavLinks.length-2].classList.remove("header-active");
+      }  else {
+        mainNavLinks[mainNavLinks.length-1].classList.remove("header-active"); 
+      }
+    });
+  });
+
+  
+
